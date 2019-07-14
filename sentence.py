210bfdea444b09dac8fefe41ae43ada607ee1463
annu1245 <annupoddar444@gmail.com>
2019-07-14 15:04:21 +0530

    Manually word tokenized

--- sentence.py
+++ sentence.py
@@ -1,7 +1,11 @@
 paragraph = "The words nowadays you probably heard of everywhere is Machine Learning, AI, Deep Learning, Blockchain. So, this blog is about what is exactly machine learning, how it has affected people lives in past decade."
 
 sentence = paragraph.split('. ')
+
 sent = []
 for sen in sentence:
     sent.append(sen.split('. '))
-print(sent)
+
+for i in range(len(sent)):
+    a = str(sent[i]).split(' ')
+    print(a)
